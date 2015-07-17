bc9373e55c3037cf4d8bf740c0ea8b653289b568
Svenja Schumann <svenja.schumann@desy.de>
2015-07-17 15:39:09 +0200

    new file

---
+++ setenv.sh
@@ -0,0 +1,11 @@
+module use -a /afs/desy.de/group/cms/modulefiles/
+module load cmssw
+module load root
+
+cd /nfs/dust/cms/user/schumas/sframe/SFramePhys14/
+source setup.sh
+cd /nfs/dust/cms/user/schumas/sframe/CMSSW_7_4_3/src/
+cmsenv
+
+
+
