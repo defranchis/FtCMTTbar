ec9b6838b814ad79d3a3c164f31efa10808a2640
schumas <svenja.schumann@desy.de>
2017-01-13 11:44:46 +0100

    script for merging the root files

---
+++ config/hadd.sh
@@ -0,0 +1,10 @@
+rm uhh2.AnalysisModuleRunner.MC.ST.root
+rm uhh2.AnalysisModuleRunner.MC.DYJetsToLL.root
+rm uhh2.AnalysisModuleRunner.MC.WJets.root
+rm uhh2.AnalysisModuleRunner.DATA.Data.root
+rm uhh2.AnalysisModuleRunner.MC.QCD.root
+hadd uhh2.AnalysisModuleRunner.MC.ST.root uhh2.AnalysisModuleRunner.MC.ST_t*
+hadd uhh2.AnalysisModuleRunner.MC.DYJetsToLL.root uhh2.AnalysisModuleRunner.MC.DYJetsToLL_M-*
+hadd uhh2.AnalysisModuleRunner.MC.WJets.root uhh2.AnalysisModuleRunner.MC.WJets1.root uhh2.AnalysisModuleRunner.MC.WJets2.root uhh2.AnalysisModuleRunner.MC.WJets3.root uhh2.AnalysisModuleRunner.MC.WJets4.root uhh2.AnalysisModuleRunner.MC.WJets5.root uhh2.AnalysisModuleRunner.MC.WJets6.root uhh2.AnalysisModuleRunner.MC.WJets7.root
+hadd uhh2.AnalysisModuleRunner.MC.QCD.root uhh2.AnalysisModuleRunner.MC.QCD1.root uhh2.AnalysisModuleRunner.MC.QCD2.root uhh2.AnalysisModuleRunner.MC.QCD3.root uhh2.AnalysisModuleRunner.MC.QCD4.root uhh2.AnalysisModuleRunner.MC.QCD5.root
+hadd uhh2.AnalysisModuleRunner.DATA.Data.root uhh2.AnalysisModuleRunner.DATA.Data*
